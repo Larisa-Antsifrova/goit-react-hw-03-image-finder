5d5234b34b1a9fde03e7a72db1c7f463a0f84bb6
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-14 10:40:57 +0200

    Added basic markup for ImageGallery and ImageGalleryItem components.

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,3 +1,9 @@
 import React from 'react';
 
 import styles from './ImageGallery.module.css';
+
+const ImageGallery = () => {
+  return <ul className="ImageGallery"></ul>;
+};
+
+export default ImageGallery;
--- src/components/ImageGalleryItem/ImageGalleryItem.js
+++ src/components/ImageGalleryItem/ImageGalleryItem.js
@@ -1,3 +1,13 @@
 import React from 'react';
 
 import styles from './ImageGalleryItem.module.css';
+
+const ImageGalleryItem = () => {
+  return (
+    <li className="ImageGalleryItem">
+      <img src="" alt="" className="ImageGalleryItem-image" />
+    </li>
+  );
+};
+
+export default ImageGalleryItem;
