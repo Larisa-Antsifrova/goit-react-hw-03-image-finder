c7c2d94ae8f8499e5d94f1db0c39a64b1211661f
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-14 10:32:02 +0200

    Added complete url.

--- src/services/pixabayApi.js
+++ src/services/pixabayApi.js
@@ -1,3 +1,5 @@
 const API_KEY = process.env.REACT_APP_API_KEY;
 const BASE_URL = 'https://pixabay.com/api/';
 const errorMessage = 'No matches were found! Try again :)';
+
+const url = `https://pixabay.com/api/?q=что_искать&page=номер_страницы&key=твой_ключ&image_type=photo&orientation=horizontal&per_page=12`;
