9ff1a19fdd9d9b274c6d6521af0a39696cdbb6e1
Larisa Antsifrova <larisa.antsifrova@gmail.com>
2021-03-15 17:02:28 +0200

    Edited css module styles.

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,7 +4,7 @@ import styles from './Button.module.css';
 
 const Button = ({ onLoadMore }) => {
   return (
-    <button type="button" className="Button" onClick={onLoadMore}>
+    <button type="button" className={styles.Button} onClick={onLoadMore}>
       Load more
     </button>
   );
